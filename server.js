2de611cc583574a82d5b3ca577417ecd4edc1529
ATHUL E R <athuler7@gmail.com>
2025-04-11 16:59:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const authRoutes = require("./routes/authroutes");  // Import auth routes
 const productRoutes = require("./routes/productRoutes"); // Import product routes
 const userAuth = require("./routes/userAuth"); // Import user auth routes
-const Regroutes = require("./routes/Regroutes");
 const profileRoutes = require("./routes/profileRoutes") // Import user registration routes
 const app = express();
 
@@ -15,7 +14,6 @@ app.use(bodyParser.json()); // Parse incoming JSON requests
 // Use auth routes for authentication (login)
 app.use("/api/auth/admin", authRoutes); // This sets the base route for auth routes
 app.use("/api/auth/user", userAuth); 
-app.use("/api/auth/reg", Regroutes);
 app.use("/api/auth", profileRoutes);
 // Use product routes for product management
 app.use("/api/products", productRoutes);
